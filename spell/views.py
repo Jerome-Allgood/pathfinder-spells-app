71599b61740fe12907cc57f49293b4a80a0edca5
Mykola Moskvin <nsmoskvin@gmail.com>
2020-04-18 17:24:57 +0300

    Basic list view added.

--- spell/views.py
+++ spell/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import ListView
+from django.views import View
+
+
+from spell.models import Spell
 
 # Create your views here.
+
+
+class TestView(ListView):
+	model = Spell
+	paginate_by = 20
+
+	def get_context_data(self, *, object_list=None, **kwargs):
+		context = super().get_context_data(**kwargs)
+		return context
+
+
+
+
+
 
 
